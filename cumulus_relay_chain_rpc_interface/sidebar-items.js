ee2ba372cdcbf82450267b0fbed0b121fe3a8c43
skunert <skunert@users.noreply.github.com>
2022-03-01 12:24:21 +0000

    deploy: 234c42c2df6f4c38e710424265beda408d210863

---
+++ cumulus_relay_chain_rpc_interface/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["RelayChainRPCInterface","RelayChainRPCInterface is used to interact with a full node that is running locally in the same process."],["Url","A parsed URL record."]]});
