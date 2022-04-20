3288ad84577f6ae4d699cc6d030cfec1d59856ee
gilescope <gilescope@users.noreply.github.com>
2022-04-20 09:51:55 +0000

    deploy: 563ce7cb93be8d57a98797dda3dc37eda5dd27f3

---
+++ parachains_common/xcm_config/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["DenyReserveTransferToRelayChain",""],["DenyThenTry","Deny executing the XCM if it matches any of the Deny filter regardless of anything else. If it passes the Deny, and matches one of the Allow cases then it is let through."]]});
