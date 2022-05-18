27667a8c40e85db50467498ed76721005cffed06
skunert <skunert@users.noreply.github.com>
2022-05-18 15:12:51 +0000

    deploy: b1a5d70256f7344a23bedaa72924b6559b187845

---
+++ test_parachain/cli/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["Commands",""]],"struct":[["ExportGenesisStateCommand",""],["ExportGenesisWasmCommand","Command for exporting the genesis wasm file."],["RelayChainCli",""],["TestCollatorCli",""]]});
---
+++ test_parachain/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["extract_genesis_wasm",""],["main",""],["wrap_announce_block",""]],"mod":[["cli",""]]});
